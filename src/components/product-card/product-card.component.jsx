17d2e97a5fa891ce6b3bfaed1dcdca8fe18f75e2
Najib Khan <naxibkhan@gmail.com>
2022-10-10 12:20:18 +0500

    add comments

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,17 +1,17 @@
-import './product-card.styles.scss';
-
-import Button from '../button/button.component';
+import "./product-card.styles.scss";
+// product cart
+import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
   return (
-    <div className='product-card-container'>
+    <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to card</Button>
+      <Button buttonType="inverted">Add to card</Button>
     </div>
   );
 };
